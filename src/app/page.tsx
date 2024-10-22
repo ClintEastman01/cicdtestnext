ba824224cd733c3a1140b5cfe26536b546a31b70
Clint Eastman <clintdev01@gmail.com>
2024-10-22 11:23:34 -0400

    added names a bit of style to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
+  const names = ["clint", "ken", "john"];
   return (
-    <h1 className="text-3xl font-bold underline">Welcome to a test page</h1>
+    <>
+      <h1 className="text-3xl font-bold underline">Welcome to a test page</h1>
+      {names.map((name, index) => (
+        <p key={`name-${index}`}>{name}</p>
+      ))}
+    </>
   );
 }
